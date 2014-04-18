affa726c6cd6cf32819c52cb93dc24040ff8ddb8
Wes Turner <wes@wrd.nu>
2014-04-18 16:49:25 -0500

    Update scripts/virtual-vagrant.sh: Compatibility with usermod -N

--- packer/scripts/virtualbox-vagrant.sh
+++ packer/scripts/virtualbox-vagrant.sh
@@ -11,7 +11,7 @@ if test -f .vbox_version ; then
     curl -kLo /home/vagrant/.ssh/authorized_keys \
       'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
     chmod 0600 /home/vagrant/.ssh/authorized_keys
-    chown -R vagrant:vagrant /home/vagrant/.ssh
+    chown -R vagrant /home/vagrant/.ssh
 
     # Install NFS client
     apt-get -y install nfs-common
