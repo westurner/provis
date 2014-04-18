ae173ea143c85a9eb909852dfaf2381181ce3b4c
Wes Turner <wes@wrd.nu>
2014-04-18 18:37:05 -0500

    Update scripts/download_ubuntu_isos.sh: checksums, <codename>-updates mini isos

--- scripts/download_ubuntu_isos.sh
+++ scripts/download_ubuntu_isos.sh
@@ -16,9 +16,14 @@ CODENAME="precise"
 
 download_ubuntu_iso_checksums() {
   version=$1
-  wget ${SERVER}/${version}/MD5SUMS -O MD5SUMS
-  wget ${SERVER}/${version}/SHA1SUMS -O SHA1SUMS
-  wget ${SERVER}/${version}/SHA256SUMS -O SHA256SUMS
+  filename="ubuntu-${version}"
+  for file in MD5SUMS SHA1SUMS SHA256SUMS; do
+    for file2 in "${file}" "${file}.gpg"; do
+      wget "${SERVER}/${version}/${file2}" -O "${filename}.${file2}"
+    done
+    echo "gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 0x<keyid>"
+    gpg --verify "${filename}.${file}.gpg" "${filename}.${file}"
+  done
 }
 
 download_ubuntu_iso() {
@@ -27,8 +32,9 @@ download_ubuntu_iso() {
   edition=$3       # desktop | server | alternate
   arch=$4          # i386 | amd64
   filename="ubuntu-${versionpoint}-${edition}-${arch}.iso"
+  checksums="ubuntu-${version}.SHA256SUMS"
   wget -c "${SERVER}/${version}/${filename}"
-  sha256sum -c <(cat SHA256SUMS | grep "${filename}")
+  sha256sum -c <(cat "${checksums}" | grep "${filename}")
 }
 
 download_ubuntu_minimal_cd_html() {
@@ -47,16 +53,61 @@ download_ubuntu_mini_iso() {
   url="/ubuntu/dists/${codename}/main/installer-${arch}/current/images/netboot/mini.iso"
   filename="ubuntu-${version}-minimal-${arch}.iso"
   wget -c "${MINIISOSERVER}/${url}" -O ${filename}
-  md5sum -c <(cat "${__md5sums}" | grep "${filename}")
+  md5sum -c <(cat "${__md5sums}" | grep "${filename}").
+}
+
+
+download_ubuntu_images_checksums() {
+  version=$1
+  codename=$2
+  arch=$3
+  filename="ubuntu-${version}-updates-${arch}-images"
+  url="ubuntu/dists/${codename}-updates/main/installer-${arch}/current/images"
+
+  for file in MD5SUMS SHA1SUMS SHA256SUMS; do
+    for file2 in "${file}" "${file}.gpg"; do
+      wget "${MINIISOSERVER}/${url}/${file2}" -O "${filename}.${file2}"
+    done
+    echo "gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 0x<keyid>"
+    gpg --verify "${filename}.${file}.gpg" "${filename}.${file}"
+  done
+}
+
+
+download_ubuntu_mini_iso_updates() {
+  codename=$1
+  version=$2
+  arch=$3
+  checksums="ubuntu-${version}-updates-${arch}-images.SHA256SUMS"
+  filename="ubuntu-${version}-minimal-${arch}.updates.iso"
+  url="ubuntu/dists/${codename}-updates/main/installer-${arch}/current/images"
+
+  checksum=$(cat "${checksums}" | grep "./netboot/mini.iso" | cut -f1 -d' ')
+  checksumfile="${filename}.SHA256SUM"
+  echo -e "${checksum}  ./${filename}" > "${checksumfile}"
+
+  wget -c "${MINIISOSERVER}/${url}/netboot/mini.iso" -O "${filename}"
+  sha256sum -c ${checksumfile}
 }
 
 main() {
   download_ubuntu_iso_checksums $VERSION
   download_ubuntu_iso $VERSION $VERSIONPOINT "alternate" "i386"
   download_ubuntu_iso $VERSION $VERSIONPOINT "alternate" "amd64"
-  download_ubuntu_minimal_cd_html "MinimalCD.html"
-  download_ubuntu_mini_iso $CODENAME $VERSION "i386"
-  download_ubuntu_mini_iso $CODENAME $VERSION "amd64"
+
+  # Download mini (netboot) ISOs
+  #download_ubuntu_minimal_cd_html "MinimalCD.html"
+  #download_ubuntu_mini_iso $CODENAME $VERSION "i386"
+  #download_ubuntu_mini_iso $CODENAME $VERSION "amd64"
+
+  # Download mini (netboot) ISOs from ${CODENAME}-updates instead
+  # see: https://bugs.launchpad.net/ubuntu/+source/net-retriever/+bug/1067934
+
+  download_ubuntu_images_checksums $VERSION $CODENAME "i386"
+  download_ubuntu_images_checksums $VERSION $CODENAME "amd64"
+  download_ubuntu_mini_iso_updates $CODENAME $VERSION "i386"
+  download_ubuntu_mini_iso_updates $CODENAME $VERSION "amd64"
+
   ls -alh .
 }
 
